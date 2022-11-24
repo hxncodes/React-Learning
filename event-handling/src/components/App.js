07bf87a36ca7be0c734e04f36819f562407f69c0
hxn Codes <hxncodes@gmail.com>
2022-11-24 12:50:02 +0500

    problem while pushing

--- event-handling/src/components/App.js
+++ event-handling/src/components/App.js
@@ -6,6 +6,11 @@ function App() {
       <header>
         <h1>Welcome</h1>
       </header>
+      <div className="container">
+        <h1>Hello</h1>
+        <input type="text" placeholder="What's your name?" />
+        <button>Submit</button>
+      </div>
     </>
   );
 }
