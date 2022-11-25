1f3f4ca8dee61a5bac82f17b85190de502ad40fa
hxnCodes <hxncodes@gmail.com>
2022-11-25 11:56:41 +0500

    Handling Mouse Over event Example

--- event-handling/src/components/App.js
+++ event-handling/src/components/App.js
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import "../App.css";
 
 function App() {
+  const [heading, setHeading] = useState("Hello");
+
+  // handling mouseOver Event
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeading("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
   return (
     <>
       <header>
-        <h1>Welcome</h1>
+        <h1>Event handling</h1>
       </header>
       <div className="container">
-        <h1>Hello</h1>
+        <h1>{heading}</h1>
         <input type="text" placeholder="What's your name?" />
-        <button>Submit</button>
+        <button
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          // Conditionaly rendering style of button
+          style={{ backgroundColor: isMouseOver ? "Black" : "red" }}
+          onClick={handleClick}
+        >
+          Submit
+        </button>
       </div>
     </>
   );
