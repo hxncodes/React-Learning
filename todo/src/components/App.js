fc867023a116236746c8e45816e356baf66f0419
hxnCodes <hxncodes@gmail.com>
2022-11-26 10:53:01 +0500

    todo - SpreadOperator Challange

---
+++ todo/src/components/App.js
@@ -0,0 +1,11 @@
+import "../App.css";
+import SpreadOperatorChallange from "./SpreadOperatorChallange";
+
+function App() {
+  return (
+    <>
+      <SpreadOperatorChallange />
+    </>
+  );
+}
+export default App;
