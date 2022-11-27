f027784e0a77e9dc73b5871a8a79fa7f80fae198
hxnCodes <hxncodes@gmail.com>
2022-11-27 21:12:04 +0500

    InputArea convereted to separate component

--- todo/src/components/ComponentTree.jsx
+++ todo/src/components/ComponentTree.jsx
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
+import InputArea from "./InputArea";
 import ToDoItem from "./ToDoItem";
 
 export default function ComponentTree() {
-  //1. When new text is written into the input, its state should be saved.
-  const [inputText, setInputText] = useState("");
-
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  //2. When the add button is pressed, the current data in the input should be
-  // Empty array using state to hold todo items
   const [items, setItems] = useState([]);
-  function addTodoItem() {
+
+  function addItem(inputText) {
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
+  }
 
-    setInputText("");
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
   }
 
   return (
@@ -26,26 +24,19 @@ export default function ComponentTree() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input type="text" value={inputText} onChange={handleChange} />
-        <button onClick={addTodoItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onAdd={addItem} />
       <div>
         <ul>
-          {/* 3. rendering todo item from items array using map function */}
-          {items.map((todoItem) => (
-            <ToDoItem text={todoItem} />
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
           ))}
         </ul>
       </div>
     </div>
   );
 }
-
-//CHALLENGE: Make this app work by applying what you've learnt.
-//1. When new text is written into the input, its state should be saved.
-//2. When the add button is pressed, the current data in the input should be
-//added to an array.
-//3. The <ul> should display all the array items as <li>s
--- todo/src/components/ToDoItem.jsx
+++ todo/src/components/ToDoItem.jsx
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
+  // const [isDone, setIsDone] = useState(false);
 
-  // it will turn false into true and true into false
-  function handleClick() {
-    setIsDone((prevValue) => {
-      return !prevValue;
-    });
-  }
+  // // it will turn false into true and true into false
+  // function handleClick() {
+  //   setIsDone((prevValue) => {
+  //     return !prevValue;
+  //   });
+  // }
   return (
-    <div onClick={handleClick}>
-      <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
-        {props.text}
-      </li>
+    // <div onClick={handleClick}>
+    //   <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+    //     {props.text}
+    //   </li>
+
+    // Props.onChecked will only be called when we click on todo item
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
   );
 }
