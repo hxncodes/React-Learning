67e270dd1cab4ba00dc7989c6174604ede9d9720
hxnCodes <hxncodes@gmail.com>
2022-11-29 21:38:54 +0500

    keeper-app step 1 done

--- keeper-app/src/components/App.js
+++ keeper-app/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -6,11 +6,25 @@ import CreateArea from "./CreateArea";
 import "../App.css";
 
 function App() {
+  // to store Multiple Notes
+  const [notes, setNotes] = useState([]);
+
+  // getting data from CreateArea.jsx and saving it to notes array
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+
+      {/* Displaying Notes from notes array using map function */}
+      {notes.map((noteItem) => {
+        return <Note title={noteItem.title} content={noteItem.content} />;
+      })}
       <Footer />
     </div>
   );
